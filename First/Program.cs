3b7fdbc80e61c61e02a3f590495af8cf1d0d92e9
AntonAntonkin <antonkin_anton@mail.ru>
2022-12-16 01:17:44 +0500

    add first task

---
+++ First/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите квадрат этого числа:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int sqr1 = Convert.ToInt32(Math.Pow(number1, 2));
+if (sqr1 == number2)
+{
+    Console.WriteLine("Число " + number2 + " является квадратом " + number1);
+}
+else 
+{
+    Console.WriteLine("Число " + number2 + " не является квадратом " + number1);
+}
